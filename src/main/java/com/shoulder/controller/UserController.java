37c75b34ae6b9814f2c936648958fa112c22cb44
wanghuadan <whd521>
2019-08-20 17:31:30 +0800

    数据加载

--- src/main/java/com/shoulder/controller/UserController.java
+++ src/main/java/com/shoulder/controller/UserController.java
@@ -1,15 +1,26 @@
 package com.shoulder.controller;
 
+import com.shoulder.model.User;
+import com.shoulder.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.List;
 
 @Controller
 @RequestMapping(value = "/user")
 public class UserController {
 
+    @Autowired
+    UserService userService;
+
     @RequestMapping(value = "/index")
-    public String userIndex() {
+    public String userIndex(Model model) {
+        List<User> userList = userService.findEntity();
+        model.addAttribute("users", userList);
         return "user/list";
     }
 }
