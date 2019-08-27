b063ed9517d7e0cb7ca89091965c39c719f2cba7
wanghuadan <1403473722@qq.com>
2019-08-27 17:22:23 +0800

    通过linkedHashMap,修改url的配置方式

---
+++ src/main/java/com/shoulder/factory/FilterChainDefinitionMapBuilder.java
@@ -0,0 +1,17 @@
+package com.shoulder.factory;
+
+import java.util.LinkedHashMap;
+
+public class FilterChainDefinitionMapBuilder {
+
+    public LinkedHashMap<String, String> buildFilterChainDefinitionMap() {
+        LinkedHashMap<String, String> map = new LinkedHashMap<String, String>();
+        map.put("/static/**", "anon");
+        map.put("/login.do", "anon");
+        map.put("/loginValidate.do", "anon");
+        map.put("/common/unauthorized.do", "anon");
+        map.put("/logout.do", "anon");
+        map.put("/**", "url");
+        return map;
+    }
+}
